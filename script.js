7b2866f9087c0dd987e107c836fb808211af19b3
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2024-01-08 16:12:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //your JS code here. If required.
-     const codeInput = document.querySelectoreAll('.code');
+      let codeInputs = document.querySelectorAll('.code');
 
-codeInput.forEach((input, i) =>{
-	if(i > 0){
-		input.addEventListener('focuse', () =>{
-			codeInput[i - 1].focuse();
-		});
-	}
-	input.addEventListener(('input',()) =>{
-		if(input.value){
-			let nextInput = codeInput[i + 1];
-			if(nextInput){
-				nextInput.focuse();
-			}else{
-				input.blur();
-			}
-		}
-	});
-});
-codeInput[0].focuse();
+        codeInputs.forEach((input, i) => {
+            if (i > 0) {
+                input.addEventListener('focus', () => {
+                    codeInputs[i - 1].focus();
+                });
+            }
+            input.addEventListener('input', () => {
+                if (input.value) {
+                    let nextInput = codeInputs[i + 1];
+                    if (nextInput) {
+                        nextInput.focus();
+                    } else {
+                        input.blur();
+                    }
+                }
+            });
+        });
+codeInputs[0].focuse();
