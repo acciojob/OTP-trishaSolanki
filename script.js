bf4ed1bd43167ea1adea4ae8436bedf1fb351ac9
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2024-01-08 13:12:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+    inputs.forEach((input, index) => {
+      input.addEventListener('input', (event) => {
+        const currentInput = event.target;
+        const maxLength = parseInt(currentInput.getAttribute('maxlength'));
+        const currentInputIndex = Array.from(inputs).indexOf(currentInput);
+
+        if (currentInput.value.length >= maxLength) {
+          if (currentInputIndex < inputs.length - 1) {
+            inputs[currentInputIndex + 1].focus();
+          }
+        }
+      });
+
+      input.addEventListener('keydown', (event) => {
+        if (event.key === 'Backspace') {
+          const currentInput = event.target;
+          const currentInputIndex = Array.from(inputs).indexOf(currentInput);
+
+          if (currentInput.value.length === 0 && currentInputIndex > 0) {
+            inputs[currentInputIndex - 1].focus();
+          }
+        }
+      });
+    });
